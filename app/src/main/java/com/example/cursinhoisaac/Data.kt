b0aadec03261f295c1da1ba62297de48ab7f331b
IsaacYusuke <129394732+IsaacYusuke@users.noreply.github.com>
2025-01-02 16:46:04 -0300

    Comentario never gonna give you up

--- app/src/main/java/com/example/cursinhoisaac/Data.kt
+++ app/src/main/java/com/example/cursinhoisaac/Data.kt
@@ -12,43 +12,43 @@ val graficos = listOf(
     GraficoData(
         titulo = "Soma e Subtração",
         arquivoJson = "SomaESubtracao.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),
     GraficoData(
         titulo = "Multiplicação",
         arquivoJson = "Multiplicacao.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Divisão",
         arquivoJson = "Divisao.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Frações",
         arquivoJson = "Fracoes.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Números Decimais",
         arquivoJson = "NumerosDecimais.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Funções",
         arquivoJson = "Funcoes.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Plano Cartesiano",
         arquivoJson = "Graficos.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Equações",
         arquivoJson = "Equacoes.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Potência e Raíz Quadrada",
         arquivoJson = "Potencia.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     ),GraficoData(
         titulo = "Fórmula de Bhaskara",
         arquivoJson = "Bhaskara.json",
-        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" //Never Gonna give you up
     )
 )
