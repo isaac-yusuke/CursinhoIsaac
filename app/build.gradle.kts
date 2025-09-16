219e795d7f218cc0b7c0f67bd7558dc6d1e6f021
IsaacYusuke <isaacyusuke@gmail.com>
2025-09-16 18:23:02 -0300

    Arrumei instruções e fiz pacote

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.isaacyyanagui.cursinhoisaac"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3      //Tem que mudar sempre que fizer nova versao
-        versionName = "1.2"  //Tem que mudar sempre que fizer nova versao
+        versionCode = 4      //Tem que mudar sempre que fizer nova versao
+        versionName = "1.5"  //Tem que mudar sempre que fizer nova versao
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/br/isaacyyanagui/cursinhoisaac/MainActivity.kt
+++ app/src/main/java/br/isaacyyanagui/cursinhoisaac/MainActivity.kt
@@ -332,7 +332,6 @@ fun Tela3() {
                             - Em cada ferramenta, você pode editar as variáveis e equações no lado esquerdo da tela para mudar os números
                             - Para retornar ao estado original, basta voltar na tela anterior e abrir novamente a ferramenta 
                             - Teste exemplos diferentes em cada aula para exercitar! Tente resolver as contas no papel e compare os resultados!
-                            - Clique no ícone do Whatsapp no canto direito da tela inicial para entrar no nosso grupo e receber todas as atualizações!
                             """.trimIndent(),
                         color = Color.White,
                         fontSize = 25.sp,
