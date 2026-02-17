5e9d7b00daee61e071af82bff58dec839eec4e70
IsaacYusuke <isaacyusuke@gmail.com>
2026-02-17 11:57:49 -0300

    Novo código de versão

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.isaacyyanagui.cursinhoisaac"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5      //Tem que mudar sempre que fizer nova versao
-        versionName = "1.6"  //Tem que mudar sempre que fizer nova versao
+        versionCode = 6      //Tem que mudar sempre que fizer nova versao
+        versionName = "1.7"  //Tem que mudar sempre que fizer nova versao
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
